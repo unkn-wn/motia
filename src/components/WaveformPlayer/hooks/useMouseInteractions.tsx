4a11d1df8aaee7ce4908d70eb410e44f4141ed28
Leon <43097991+unkn-wn@users.noreply.github.com>
2026-01-03 13:02:57 -0800

    Update useMouseInteractions.tsx

--- src/components/WaveformPlayer/hooks/useMouseInteractions.tsx
+++ src/components/WaveformPlayer/hooks/useMouseInteractions.tsx
@@ -157,7 +157,7 @@ export const useMouseInteractions = () => {
 		const getZoomFactors = (e: WheelEvent) => {
 			// Mac Pinch (CtrlKey) fires high frequency events -> Low Sensitivity
 			if (isMac && e.ctrlKey) {
-				return { in: 1.01, out: 0.99 };
+				return { in: 1.04, out: 0.96 };
 			}
 			// Mouse Wheel / PC -> Standard Sensitivity
 			return { in: 1.1, out: 0.9 };
